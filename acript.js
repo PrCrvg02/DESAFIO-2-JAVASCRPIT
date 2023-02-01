26aa13d4ad980231c2cfaca1339bb96965f1db4c
PrCrvg <paulor1516@outlook.com>
2023-01-31 21:45:50 -0300

    DESAFIO-2-JAVASCRIPT

---
+++ acript.js
@@ -0,0 +1,40 @@
+function mensagem(){
+    console.log("Bem vindo ao meu desafio 2 de javascript")
+}
+
+mensagem()
+
+function nome(name){
+    console.log(name)
+
+}
+
+nome("Paulo Ricardo")
+
+function bio(nome,idade,musica){
+    console.log(nome,idade,musica)
+}
+
+bio("Paulo Ricardo", 19, "rap")
+
+function favoritos(filme,musicaBoa){
+    console.log(filme,musicaBoa)
+}
+
+favoritos("Carros", "Cicatrizes(Orochi)")
+
+function triplo(valor){
+    console.log(valor*3)
+}
+
+triplo(250)
+
+let numero = true
+
+function verificar(variavel){
+    if(numero==true){
+        console.log(numero)
+    }
+}
+
+verificar()
